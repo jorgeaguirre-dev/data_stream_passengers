636e85e67267d15309a083a1c7a04496cf990548
jorgeaguirre-dev <jorgeaguirre.tech@gmail.com>
2026-03-21 00:33:48 -0300

    organized files in news folders

---
+++ infra/main.tf
@@ -0,0 +1,35 @@
+# Definición del Proveedor
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  zone  = var.zone
+}
+
+# 1. Pub/Sub para Ingesta de Eventos
+resource "google_pubsub_topic" "flight_searches" {
+  name = var.pubsub_topic_name
+}
+
+# 2. Dataset de BigQuery (Capa Raw y Analytics)
+resource "google_bigquery_dataset" "latam_ds" {
+  dataset_id = var.bigquery_dataset_id
+  location   = var.location
+}
+
+# 3. Bucket Cloud Storage de Staging para Dataflow
+resource "google_storage_bucket" "dataflow_staging" {
+  name          = var.dataflow_staging_bucket
+  location      = var.location
+  force_destroy = true
+}
+
+# 4. Job de Dataflow (Streaming)
+resource "google_dataflow_job" "streaming_segmentation" {
+  name              = var.dataflow_job_name
+  template_gcs_path = "gs://dataflow-templates/latest/PubSub_to_BigQuery"
+  temp_gcs_location = "${google_storage_bucket.dataflow_staging.url}/temp"
+  parameters = {
+    inputTopic      = google_pubsub_topic.flight_searches.id
+    outputTableSpec = "${var.project_id}:${google_bigquery_dataset.latam_ds.dataset_id}.raw_events"
+  }
+}
---
+++ infra/vars.tf
@@ -0,0 +1,47 @@
+variable "project_id" {
+  description = "GCP Project ID"
+  type        = string
+  default     = "data-stream-passengers"
+}
+
+variable "region" {
+  description = "GCP region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "zone" {
+  description = "GCP zone"
+  type        = string
+  default     = "us-central1-a"
+}
+
+variable "location" {
+  description = "GCP resource location"
+  type        = string
+  default     = "US"
+}
+
+variable "pubsub_topic_name" {
+  description = "Nombre del tópico Pub/Sub"
+  type        = string
+  default     = "flight-search-events"
+}
+
+variable "bigquery_dataset_id" {
+  description = "ID del dataset de BigQuery"
+  type        = string
+  default     = "passenger_segmentation"
+}
+
+variable "dataflow_staging_bucket" {
+  description = "Nombre del bucket de staging para Dataflow"
+  type        = string
+  default     = "dataflow-staging-bucket"
+}
+
+variable "dataflow_job_name" {
+  description = "Nombre del job de Dataflow"
+  type        = string
+  default     = "segmentation-pipeline"
+}
